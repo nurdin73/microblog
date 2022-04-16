cfe74ec1c55be2c45253ba2f23b51ea2413e3478
nurdin73 <nurdin.reverse73@gmail.com>
2022-04-16 11:58:58 +0700

    add endpoint documentation

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,67 @@ Route::get('/', function () {
     return response([
         'message' => 'Welcome to the API. this is base url for api',
         'status' => 'success',
-        'version' => '1.0.0'
+        'version' => '1.0.0',
+        'endpoint' => [
+            [
+                'title' => 'Blogs',
+                'url' => url('api/blogs'),
+                'method' => 'GET',
+                'body' => null,
+                'params' => null,
+                'query' => [
+                    'search' => 'string',
+                    'limit' => 'integer',
+                    'by' => 'string',
+                    'order' => 'string',
+                    'aditional' => 'string',
+                ],
+            ],
+            [
+                'title' => 'Blog Detail',
+                'url' => url('api/blogs/{id}'),
+                'method' => 'GET',
+                'body' => null,
+                'params' => [
+                    'id' => 'integer',
+                ],
+                'query' => [
+                    'customer_id' => 'integer',
+                ],
+            ],
+            [
+                'title' => 'Like/Unlike Blog',
+                'url' => url('api/blogs/like-unlike'),
+                'method' => 'POST',
+                'body' => [
+                    'blog_id' => 'integer',
+                    'customer_id' => 'integer',
+                ],
+                'params' => null,
+                'query' => null,
+            ],
+            [
+                'title' => 'Quotes/Funfacts',
+                'url' => url('api/quote-funfacts'),
+                'method' => 'GET',
+                'body' => null,
+                'params' => null,
+                'query' => [
+                    'limit' => 'integer',
+                ],
+            ],
+            [
+                'title' => 'Collections',
+                'url' => url('api/collections'),
+                'method' => 'GET',
+                'body' => null,
+                'params' => null,
+                'query' => [
+                    'search' => 'string',
+                    'limit' => 'integer',
+                ],
+            ]
+        ]
     ]);
 });
 Route::group(['prefix' => 'blogs'], function() {
