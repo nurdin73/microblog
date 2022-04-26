15e46b41e4dac1665584ff6d2418c1fd59936c5c
nurdin73 <nurdin.reverse73@gmail.com>
2022-04-26 15:51:52 +0700

    bug reset password

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -2,16 +2,26 @@
 @section('title', 'Reset password')
 @section('content')
   <div class="w-full">
-    @if (session('status'))
+    @error('email')
       <div class="flex items-center justify-between p-4 mb-8 text-sm font-semibold text-purple-100 bg-purple-600 rounded-lg shadow-md focus:outline-none focus:shadow-outline-purple">
         <div class="flex items-center">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
-          <span>{{ session('status') }}</span>
+          <span>{{ $message }}</span>
         </div>
       </div>
-    @endif
+    @enderror
+    @error('token')
+      <div class="flex items-center justify-between p-4 mb-8 text-sm font-semibold text-purple-100 bg-purple-600 rounded-lg shadow-md focus:outline-none focus:shadow-outline-purple">
+        <div class="flex items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+          <span>{{ $message }}</span>
+        </div>
+      </div>
+    @enderror
     <form action="{{ route('password.update') }}" method="post">
       @csrf
       <input type="hidden" name="token" value="{{ $token }}">
